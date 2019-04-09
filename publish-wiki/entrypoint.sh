d08cde8a03d5286b5d35e7028aeff2f36a06b13f
Mindsers <dev@nathanaelcherrier.com>
2019-04-09 11:30:23 +0200

    Name the variables

--- publish-wiki/entrypoint.sh
+++ publish-wiki/entrypoint.sh
@@ -2,33 +2,40 @@
 
 set -euo pipefail
 
-doc_path=$*
-
 function configureGit {
-    profile="$(curl -s -H "Authorization: token $1" "https://api.github.com/users/$2")"
-    git config --global user.name "$(printf '%s' "$profile" | jq -r .name)"
-    git config --global user.email "$(printf '%s' "$profile" | jq -r .email)"
+    token=$1
+    actor=$2
+
+    profile="$(curl -s -H "Authorization: token ${token}" "https://api.github.com/users/${actor}")"
+    git config --global user.name "$(printf '%s' "${profile}" | jq -r .name)"
+    git config --global user.email "$(printf '%s' "${profile}" | jq -r .email)"
 }
 
 function configureSsh {
+    key=$1
+
     echo "github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==" >>/etc/ssh/ssh_known_hosts
     eval "$(ssh-agent -s)" >/dev/null
     mkfifo -m 600 ~/.ssh_key.fifo
-    printf -- "$1\n" >~/.ssh_key.fifo | ssh-add ~/.ssh_key.fifo
+    printf -- "${key}\n" >~/.ssh_key.fifo | ssh-add ~/.ssh_key.fifo
     rm ~/.ssh_key.fifo
 }
 
 function updateWiki {
-    echo "Wiki update starts..."
-    wiki_repo="git@github.com:$2.wiki.git"
+    doc_path=$1
+    wiki_repo_name=$2
+    commit_message=$3
 
-    echo "Uploading $1 content to ${wiki_repo}"
+    echo "Wiki update starts..."
+    wiki_repo="git@github.com:${wiki_repo_name}.wiki.git"
+
+    echo "Uploading ${doc_path} content to ${wiki_repo}"
     git clone ${wiki_repo} wiki_folder_temp
     rm -rf wiki_folder_temp/*
 
-    cp -rv $1/* wiki_folder_temp
+    cp -rv ${doc_path} /* wiki_folder_temp
     cd wiki_folder_temp
-    git commit -am $3
+    git commit -am ${commit_message}
     git push
 }
 
@@ -40,6 +47,6 @@ fi
 configureSsh ${SSH_KEY}
 configureGit ${GITHUB_TOKEN} ${GITHUB_ACTOR}
 
-updateWiki ${doc_path} ${GITHUB_REPOSITORY} "${GITHUB_ACTOR} - ${GITHUB_SHA}"
+updateWiki $* ${GITHUB_REPOSITORY} "${GITHUB_ACTOR} - ${GITHUB_SHA}"
 
 exit 0
