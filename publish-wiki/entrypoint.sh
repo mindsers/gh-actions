c79dfb2aa277fb9b9cda3761323d38d91b4968f7
Mindsers <dev@nathanaelcherrier.com>
2019-04-09 11:33:35 +0200

    Move exit statement in function

--- publish-wiki/entrypoint.sh
+++ publish-wiki/entrypoint.sh
@@ -6,6 +6,11 @@ function configureGit {
     token=$1
     actor=$2
 
+    if ! [[ -n "${SSH_KEY:-}" ]]; then
+        (>&2 echo "ERROR. SSH_KEY is mandatory.")
+        exit 1
+    fi
+
     profile="$(curl -s -H "Authorization: token ${token}" "https://api.github.com/users/${actor}")"
     git config --global user.name "$(printf '%s' "${profile}" | jq -r .name)"
     git config --global user.email "$(printf '%s' "${profile}" | jq -r .email)"
@@ -13,6 +18,11 @@ function configureGit {
 
 function configureSsh {
     key=$1
+
+    if ! [[ -n "${GITHUB_TOKEN:-}" ]]; then
+        (>&2 echo "ERROR. GITHUB_TOKEN are mandatory.")
+        exit 1
+    fi
 
     echo "github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==" >>/etc/ssh/ssh_known_hosts
     eval "$(ssh-agent -s)" >/dev/null
@@ -37,16 +47,11 @@ function updateWiki {
     cd wiki_folder_temp
     git commit -am ${commit_message}
     git push
-}
 
-if ! [[ -n "${SSH_KEY:-}" ]] || ! [[ -n "${GITHUB_TOKEN:-}" ]]; then
-    (>&2 echo "ERROR. SSH_KEY and GITHUB_TOKEN are mandatory.")
-    exit 1
-fi
+    exit 0
+}
 
 configureSsh ${SSH_KEY}
 configureGit ${GITHUB_TOKEN} ${GITHUB_ACTOR}
 
 updateWiki $* ${GITHUB_REPOSITORY} "${GITHUB_ACTOR} - ${GITHUB_SHA}"
-
-exit 0
